90d630940fa4f7e68f500ddcf0d5590a856e81e3
Your Name <kabi10shrestha@yahoo.com>
2019-12-02 14:49:28 +0700

    Updated csv reading program

--- csvread.cpp
+++ csvread.cpp
@@ -5,7 +5,90 @@
 
 using namespace std;
 
+/** brief csvtovector - input of csv string split into vector
+ *
+ * param string n csv string with '"' included
+ * "1,101,Alan Smith,\"Austin, Texas\",77,hello"
+ * return vector<string>
+ */
+vector<string> csvtovector(string n) {
+	int pos = 0;
+	vector<string> csv_vector;
+	if (n[pos] == '\"') { //special case where first element starts with '"'
+		pos = (n.find("\"", pos + 1) + 1);
+		csv_vector.push_back(n.substr(0, pos));
+		pos += 1;
+	}
+	size_t found = n.find(",", pos);
+	while (found != string::npos) {
+		//cout << n.substr(pos, (found - pos)) << endl;
+		csv_vector.push_back(n.substr(pos, (found - pos)));
+		pos = (found + 1);
+		if (n[pos] == '\"') {
+			found = n.find("\"", pos + 1);
+			csv_vector.push_back(n.substr(pos, ((found + 1) - pos)));
+			pos = found + 2;
+		}
+		found = n.find(",", pos + 1);
+	}
+	csv_vector.push_back(n.substr(pos));
+	return csv_vector;
+}
+/** brief print_matching_column - uses input header name and prints specific column from csv data
+ *
+ * param column_name header column to be printed from each csv data
+ * param headers contains header names
+ * param dbdata contains database data lines
+ * if header name does not match then nothing is printed
+ * 
+ */
+void print_matching_column(string column_name, vector<string> headers, vector<string> dbdata) {
+	for (int i = 0; i < headers.size(); i++) {
+		if (headers[i] == column_name) {
+			cout << "Found Header: " << headers.at(i) << endl;
+			for (int j = 0; j < dbdata.size(); j++) {
+				vector<string> dbline = csvtovector(dbdata[j]);
+				cout << dbline[i] << endl;
+			}
+		}
+	}
+}
+
+/** brief print_matching_row - uses input header name and value to print matching csv data rows
+ *
+ * param column_name header column to be matched from each csv data
+ * param column_value value to be matched in data line
+ * param headers contains header names
+ * param dbdata contains database data lines
+ * if header name does not match then nothing is printed
+ *
+ */
+void print_matching_row(string column_name, string column_value, vector<string> headers, vector<string> dbdata) {
+	for (int i = 0; i < headers.size(); i++) {
+		if (headers[i] == column_name) {
+			cout << "Found Header: " << headers.at(i) << endl;
+			for (int j = 0; j < dbdata.size(); j++) {
+				vector<string> dbline = csvtovector(dbdata[j]);
+				if (column_value == dbline[i]) {
+					cout << dbdata[j] << endl;
+				}
+			}
+		}
+	}
+}
+
+/** brief main - command line argument searching csv file by headers and matching parameter
+ *
+ * param int argc contains the number of command-line arguments passed by user
+ * param char* argv[] contains the strings matching the header names or matching the header name and parameter
+ * csvtovector sex
+ * prints all elements of column sex
+ * csvtovector sex=F
+ * prints all lines where sex column = F
+ * return 0
+ */
 int main(int argc, char* argv[]) {
+
 	string filedata;
 	int pos = 0;
 	vector<string> headers;
@@ -18,13 +101,9 @@ int main(int argc, char* argv[]) {
 	}
 	if (!inFile) { cout << "Error in opening file.\n"; return 1; }
 	getline(inFile, filedata);
-	size_t found = filedata.find(",");
-	while (found != string::npos) {
-		headers.push_back(filedata.substr(pos, (found-pos)));
-		pos = (found + 1);
-		found = filedata.find(",", pos+1);
-	}
-	headers.push_back(filedata.substr(pos));
+
+	
+	headers = csvtovector(filedata);
 
 	while (!inFile.eof()) {
 		getline(inFile, filedata);
@@ -32,47 +111,20 @@ int main(int argc, char* argv[]) {
 	}
 	inFile.close();
 
-	/*for (int i = 0; i < headers.size(); i++)
-		cout << headers.at(i) << " ";
-	for (int i = 0; i < dbdata.size(); i++)
-		cout << dbdata.at(i) << endl;
-	*/
-	
-	string header_search;
 	string find_header = argv[1];
 	size_t foundeq = find_header.find("=");
+
 	string header_match = find_header.substr(0, foundeq);
 	string para_match = find_header.substr(foundeq + 1);
-	if (foundeq != string::npos)
-		header_search = header_match;
-	else
-		header_search = find_header;
-	   
-	for (int i = 0; i < headers.size(); i++) {
-		if (headers[i] == header_search) {
-			cout << "Found Header: " << headers.at(i) << endl;
-			for (int j = 0; j < dbdata.size(); j++) {
-				size_t found = dbdata[j].find(",");
-				int pos1 = 0;
-				while (found != string::npos) {
-					temp_data.push_back(dbdata[j].substr(pos1, (found - pos1)));
-					pos1 = (found + 1);
-					found = dbdata[j].find(",", pos1 + 1);
-				}
-				temp_data.push_back(dbdata[j].substr(pos1));
-				if (temp_data[i] == para_match) {
-					for (int k = 0; k < temp_data.size(); k++)
-						cout << temp_data.at(k) << ", ";
-					cout << endl;
-				}
-				if (foundeq == string::npos)
-					cout << temp_data[i] << endl;
-				temp_data.clear();
-			}
-		}
+
+	if (foundeq != string::npos) {
+		print_matching_row(header_match, para_match, headers, dbdata);
 	}
-			
-	system("pause");
+	else {
+		print_matching_column(find_header, headers, dbdata);
+	}
+		
+	//system("pause");
 	return 0;
 }
 
